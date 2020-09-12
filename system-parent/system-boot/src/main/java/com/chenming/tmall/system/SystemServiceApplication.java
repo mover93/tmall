d8ebc688df2149bae4b82d6ba6b24cf59d4f6ad2
chenming <mover93@126.com>
2020-09-12 10:00:38 +0800

    system模块搭建以及自测

---
+++ system-parent/system-boot/src/main/java/com/chenming/tmall/system/SystemServiceApplication.java
@@ -0,0 +1,18 @@
+package com.chenming.tmall.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description: SystemServiceApplication <br>
+ * author: chenming <br>
+ * date: 2020/9/12 9:51 <br>
+ * version: 1.0 <br>
+ */
+@SpringBootApplication
+public class SystemServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemServiceApplication.class, args);
+    }
+}
---
+++ system-parent/system-boot/src/test/java/com/chenming/tmall/system/SystemServiceApplicationTests.java
@@ -0,0 +1,21 @@
+package com.chenming.tmall.system;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * description: SystemServiceApplicationTests <br>
+ * author: chenming <br>
+ * date: 2020/9/12 9:53 <br>
+ * version: 1.0 <br>
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SystemServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
